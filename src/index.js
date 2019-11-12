fb39bd7e740026b88b1050b84b57f1dc805624fc
Radosław Grzymała <radoslaw.grzymala@sensilabs.pl>
2019-11-12 19:29:26 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios";
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+class Reddit extends React.Component {
+    state = {
+      posts: []
+    };
+  
+    componentDidMount() {
+      axios.get(`https://www.reddit.com/r/reactjs.json`).then(res => {
+        const posts = res.data.data.children.map(obj => obj.data);
+        this.setState({ posts });
+      });
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1>/r/reactjs</h1>
+          <ul>
+            {this.state.posts.map(post => {
+              return <li key={post.id}>{post.title}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    }
+  }
+   
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
